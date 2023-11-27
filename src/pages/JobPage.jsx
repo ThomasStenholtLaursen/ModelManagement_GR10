8db24dfd431fe8c56caac1d4397d140bc7e44330
SamuelJessen <201906916@post.au.dk>
2023-11-27 21:11:38 +0100

    Add functionality to add models to job

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useAuth } from "../hooks/useAuth";
 import { Box, Breadcrumbs, Button, Typography } from "@mui/material";
 import { Link, useNavigate, useParams } from "react-router-dom";
@@ -9,6 +9,7 @@ import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import AddModelToJobModal from "../components/Job/AddModelToJobModal";
 import ExspensesTable from "../components/Job/ExpensesTable";
 import ModelsOnJobTable from "../components/Job/ModelsOnJobTable";
+import useAddModelToJob from "../hooks/useAddModelToJob";
 
 const JobPage = () => {
     const {jobId} = useParams()
@@ -18,10 +19,12 @@ const JobPage = () => {
     const {fetchData : fetchJobData, error } = useFetchJob(token);
     const {fetchData : fetchExpenseData} = useFetchExpenses(token);
     const {fetchData : fetchModelsData} = useFetchModels(token);
+    const {addModelToJob} = useAddModelToJob(token);
     const [job, setJob] = useState();
     const [expenses, setExpenses] = useState([]);
     const [models, setModels] = useState([]);
     const [isModalOpen, setModalOpen] = useState(false);
+    const [modelsWithoutThisJob, setModelsWithoutThisJob] = useState([]);
 
     const handleOpenAddModelToJobModal = () => {
         setModalOpen(true);
@@ -34,18 +37,31 @@ const JobPage = () => {
     const handleOpenExpenseModal = () => {
         // Implement logic to open the AddExpenseModal
     }
-    const handleAddModelToJob = (selectedModel) => {
-        // Implement logic to add the selectedModel to your job
-        console.log("Selected Model:", selectedModel);
-    };
     
-    useEffect(() => {
-      const loadJob = async () => {
+    const loadModels = useCallback(async () => {
+        const modelsData = await fetchModelsData();
+        if (modelsData) {
+            setModels(modelsData);
+        }
+    }, [fetchModelsData]);
+
+    const loadJob = useCallback( async () => {
         const jobData = await fetchJobData(jobId);
         if (jobData) {
           setJob(jobData);
         }
-      };
+      }, [fetchJobData, jobId]); 
+    
+
+    useEffect(() => {
+        const modelEmails = job?.models.map((model) => model.email) || [];
+        const modelsWithoutThisJob = models.filter((model) => !modelEmails.includes(model.email));
+        setModelsWithoutThisJob(modelsWithoutThisJob)
+        loadModels();
+        loadJob();
+    }, [job?.models, loadJob, loadModels, models]);
+    
+    useEffect(() => {
       const loadExpenses = async () => {
         const expensesData = await fetchExpenseData();
         if (expensesData) {
@@ -58,23 +74,20 @@ const JobPage = () => {
             }
         }
       };
-
-      const loadModels = async () => {
-        const modelsData = await fetchModelsData();
-        if (modelsData) {
-          setModels(modelsData);
-        }
-      }
-  
-      loadJob();
       loadExpenses();
-      loadModels();
-    }, [fetchExpenseData, fetchJobData, fetchModelsData, isManager, jobId, user.id]);
+    }, [fetchExpenseData, isManager, jobId, user.id]);
 
+    const handleAddModelToJob = async (selectedModel, job) => {
+        const result = await addModelToJob(selectedModel.value.efModelId, job.jobId);
+        if (result) {
+            handleCloseModal();
+            loadJob()
+        }
+    };
+    
     const handleJobsLinkClick = () => {
         navigate(-1);
       };
-
   
     if (error) return <div>Error: {error}</div>;
   
@@ -108,10 +121,10 @@ const JobPage = () => {
           <Typography variant="h4">Expenses</Typography>
           <ExspensesTable expenses={expenses} models={models} />
           <AddModelToJobModal
-            models={models}
+            models={modelsWithoutThisJob}
             open={isModalOpen}
             onClose={handleCloseModal}
-            onAdd={handleAddModelToJob}
+            onAdd={(selectedModel) => handleAddModelToJob(selectedModel, job)}
             />
         </>
       );
